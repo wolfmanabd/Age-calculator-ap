c74164d58b41020cd14706cc5b1624da2d35a2bf
wolfman <wolfmanabd@gmail.com>
2023-10-21 22:25:05 +0100

    finished with design

---
+++ script.js
@@ -0,0 +1,4 @@
+const inputDay = document.querySelectorAll("#input-day");
+const inputMonth = document.querySelectorAll("#input-month");
+const inputYear = document.querySelectorAll("#input-year");
+const allInput = document.querySelectorAll(".input");
